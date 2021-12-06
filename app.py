6b78ff95460f52f99f4784c4d6d7ea3c11db7ee3
velllum <velllum@yandex.ru>
2021-12-07 00:58:41 +0300

    Исправление ошибок

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 from application import create_app
-from application.config.settings import settings
+
 
 app = create_app()
 
@@ -10,8 +10,8 @@ conf = app.state.config
 if __name__ == "__main__":
     uvicorn.run(
         'app:app',
-        host=settings.host,
-        port=settings.port,
+        host=conf.host,
+        port=conf.port,
         reload=True,
         debug=True,
         log_config='application/config/logging.conf'
